f7ffe0ca017b19e961c16875b766e3a56aaa9457
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-08-22 13:29:34 +0100

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,31 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn some_function() {
+        println!("Let's Get Rusty!");
+    }
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let localhost = IpAddrKind::V4(127,0,0,1);
+}
+
+fn route(ip_kind: IpAddrKind) {}
