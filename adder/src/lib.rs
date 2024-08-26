664694dfc3aad25cb110229bf475d812ef6294bb
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-08-26 18:20:01 +0100

    basic tests

---
+++ adder/src/lib.rs
@@ -0,0 +1,98 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_can_not_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn it_adds_two_not_equal() {
+        assert_ne!(3, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+    }
+
+    #[test]
+    fn greeting_not_contains_name() {
+        let result = greeting("Carol");
+        let wrong_name = "Wale";
+        assert!(
+            result.contains(wrong_name),
+            "Greeting did not contain name value `{}`",
+            wrong_name
+        );
+    }
+
+    #[test]
+    fn it_works_again() -> Result<(), String> {
+        if 2 + 3 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus three does not equal 4"))
+        }
+    }
+}
